e7286f0c0dab1e0ec5720fb2baf34ba3e2cbdcd8
fenix15100 <francisco.camacho15@gmail.com>
2018-03-20 19:53:14 +0100

    acabar

--- PR01 - Primers passos amb dotnet core/PR01 - Primers passos amb dotnet core/Controllers/AccountController.cs
+++ PR01 - Primers passos amb dotnet core/PR01 - Primers passos amb dotnet core/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Novell.Directory.Ldap;
 using PR01___Primers_passos_amb_dotnet_core.Models;
 
 namespace PR01___Primers_passos_amb_dotnet_core.Controllers
@@ -13,14 +15,60 @@ namespace PR01___Primers_passos_amb_dotnet_core.Controllers
             
             return View();
         }
-        
+
         [HttpPost]
         public IActionResult Login(LoginDto loginDto)
         {
-            //todo CheckLdap(loginDto) if true meto en sesion y retorno viesta home si no meto en sesion error y retorno a form
-            // alli handeo la ssesion para inyectar el error.
-            HttpContext.Session.SetString("user", loginDto.FirstName);
-            return RedirectToAction("index", "Home");
+            
+            if (CheckLdap(loginDto))
+            {
+                HttpContext.Session.SetString("user", loginDto.FirstName);
+                return RedirectToAction("index", "Home");   
+            }
+            
+            HttpContext.Session.SetString("error", "Fallo la Autentificacion");
+            return RedirectToAction("Login", "Account"); 
+                
+            
+
+
+        }
+
+
+
+        public bool CheckLdap(LoginDto loginDto)
+        {
+            
+            var ldapConn = new LdapConnection();
+            ldapConn.Connect("192.168.123.240", 389);
+
+            try
+            {
+               ldapConn.Bind(@"cn="+loginDto.FirstName+",dc=fran,dc=local", loginDto.Password);
+                return true;
+            }catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+                return false;
+            }
+           
+         
+            
+        }
+
+
+        public LdapSearchResults GetResult()
+        {   
+            var ldapConn = new LdapConnection();
+            ldapConn.Connect("192.168.123.240", 389);
+            
+            string filter = "(ObjectClass=*)";
+            
+            var query = ldapConn.Search("dc=fran,dc=local", LdapConnection.SCOPE_SUB, filter, null, false);
+
+            return query;
+
         }
 
         
