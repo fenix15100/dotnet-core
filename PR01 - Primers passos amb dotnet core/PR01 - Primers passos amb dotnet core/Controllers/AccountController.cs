d72b47fb3110c57e655f72928feb040fa749b21f
fenix15100 <francisco.camacho15@gmail.com>
2018-04-01 10:57:58 +0200

    done

--- PR01 - Primers passos amb dotnet core/PR01 - Primers passos amb dotnet core/Controllers/AccountController.cs
+++ PR01 - Primers passos amb dotnet core/PR01 - Primers passos amb dotnet core/Controllers/AccountController.cs
@@ -53,17 +53,22 @@ namespace PR01___Primers_passos_amb_dotnet_core.Controllers
 
         public bool CheckLdap(LoginDto loginDto)
         {
+            //Creamos el objeto Ldap
+            LdapConnection ldapConn = new LdapConnection();
             
-            var ldapConn = new LdapConnection();
-            ldapConn.Connect("192.168.1.101", 389);
+            //Abrimos el Socket contra el servidor Ldap
+            ldapConn.Connect("192.168.1.102", 389);
 
+            
             try
             {
+               //Intentamos autentificarnos contra el dominio Ldap, utilizando los datos del objeto LoginDto
+               //Si no salta la excepcion retornamos true si no es asi retornamos false
                ldapConn.Bind(@"cn="+loginDto.FirstName+",ou=Users,dc=fran,dc=local", loginDto.Password);
                 return true;
             }catch (LdapException e)
             {
-
+                //Debug
                 Console.WriteLine(e);
                 return false;
             }
@@ -75,48 +80,59 @@ namespace PR01___Primers_passos_amb_dotnet_core.Controllers
 
         public IActionResult GetUsersLdap()
         {
+            //Listas que utilizare para almacenar los usuarios de Ldap y sus respectivos atributos
             List<String> ldapusers= new List<String>();
             List<String> atributos= new List<String>();
             
             
-            
-           var ldapConn = new LdapConnection();
-           ldapConn.Connect("192.168.1.101", 389);
+           
+            //Abrimos Socket contra el server
+           LdapConnection ldapConn = new LdapConnection();
+           ldapConn.Connect("192.168.1.102", 389);
         
+            //Definimos un filtro Ldap utilizado mas tarde, en este caso solo queremos los objetos tipo usuario
            string filter = "(ObjectClass=inetOrgPerson)";
 
 
             try{
-                LdapSearchResults query = ldapConn.Search("ou=Users,dc=fran,dc=local", LdapConnection.SCOPE_SUB, filter, null, false);
+                //Hacemos la busqueda
+                LdapSearchResults query = ldapConn.Search("dc=fran,dc=local", LdapConnection.SCOPE_SUB, filter, null, false);
                 
                 
+                //Recorremos la colecion de objetos Ldap
                 while (query.hasMore()){
                     
                     
                     
                     try {
+                        //Obtenemos el usuario iterado y obtenemos su DN y los metemos en la lista, hacemos lo mismo con
+                        //TODOS sus atributos(Por comodidad he hecho un toString)
+                        //En vez de recorrorerlos 1 a 1.
+                        
                         LdapEntry nextEntry =query.next();
                         ldapusers.Add(nextEntry.DN);
                         atributos.Add(nextEntry.getAttributeSet().ToString());                 
 
                     }catch(LdapException e) {
+                        //Si hubiera algun fallo el la obtencion del usuario iterado lo logeo y continuo
                         Console.WriteLine("Error Entry: " + e.LdapErrorMessage);
                         
                     }
                 }
             }catch (LdapException e){
+                //Si la busqueda ha fallado lo logueo y lanzo una excepcion para parar la app
                 Console.WriteLine("Error Filtro: "+e.LdapErrorMessage);
                 throw;
             }
-           
             
-          
-               
+            //Cargo las dos listas en la array dinamica de la vista
+             
             ViewBag.ldapusers = ldapusers;
             ViewBag.atributos = atributos;
             return View();
 
         }
+
 
 
 
